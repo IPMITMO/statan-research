1506f8c726a6adb411f747d83241e51991bd4412
artemvirused <getviruses@gmail.com>
2017-01-28 19:17:51 +0300

    Coala time test of requests project #66

--- analyze/perfomance_test/coala_requests_time_test.py
+++ analyze/perfomance_test/coala_requests_time_test.py
@@ -6,7 +6,7 @@ count = 10
 
 for i in range(0,count):
     with Timer() as t:
-        os.system("pylama -i C0111 -l \"pylint,pyflakes\" ../../sources/requests-2.12.1 > /dev/null")
+        os.system("coala --json > /dev/null")
     print("=> elasped pylama: %s s" % t.secs)
     time_sum = time_sum + t.secs
 print("Average: " + str(time_sum / count) + " s")
